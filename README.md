a54d77563a25e06d3299db34e9b2d9c9a52997fb
nickar12 <nick@rambos.co.uk>
2018-05-05 14:01:27 +0100

    added dropbox uploader. removing file with command line not node for now

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-twitter-destructor
 A node version of the twitter destructor by @reyhan.
+
+# Other dependencies
+
+(Dropbox Uploader)[https://github.com/andreafabrizi/Dropbox-Uploader].
--- destruct.js
+++ destruct.js
@@ -11,24 +11,28 @@ const getParams = {
 };
 const now = moment().format('YYYYMMDD_HHmmss');
 
+terminal.stdout.on('data', data => console.log(`stdout: ${data}`));
+terminal.on('exit', code => console.log(`child process exited with code   ${code}`));
+
 client.get('statuses/user_timeline', getParams, (error, tweets, response) => {
   if (!error) {
-    if (!fs.existsSync(`./${getParams.screen_name}`)) {
-      fs.mkdirSync(`./${getParams.screen_name}`);
-    }
-    fs.appendFile(`./${getParams.screen_name}_${now}.json`, JSON.stringify(tweets), (err) => {
+    fs.appendFileSync('tweets.json', JSON.stringify(tweets), (err) => {
       if (err) {
         console.log(err);
       }
     });
+    terminal.stdin.write(`./Dropbox-Uploader/dropbox_uploader.sh upload ./tweets.json /${getParams.screen_name}_${now}.json\n`);
+    terminal.stdin.write('rm tweets.json');
+    terminal.stdin.end();
   } else {
     console.log(error);
   }
+
+  // fs.unlink('tweets.json', (err) => {
+  //   console.log('got here');
+  //   if (err) {
+  //     console.log('unlink error');
+  //     console.log(err);
+  //   }
+  // });
 });
-
-terminal.stdout.on('data', data => console.log(`stdout:  + ${data}`));
-
-terminal.on('exit', code => console.log(`child process exited with code   ${code}`));
-
-terminal.stdin.write('git status\n');
-terminal.stdin.end();
