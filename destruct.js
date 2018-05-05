584b9678b4994a8ed3e7aaf7d06cd20d10273a12
nickar12 <nick@rambos.co.uk>
2018-05-05 12:11:46 +0100

    query to twitter api is working, saving to local directory

--- destruct.js
+++ destruct.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 const Twitter = require('twitter');
+const moment = require('moment');
 const credentials = require('./credentials');
 
 const client = new Twitter(credentials);
+const getParams = {
+  screen_name: 'nickramsbottom',
+  count: 300,
+};
+const now = moment().format('YYYYMMDD_HHmmss');
 
-const params = { screen_name: 'nickramsbottom' };
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
+client.get('statuses/user_timeline', getParams, (error, tweets, response) => {
   if (!error) {
-    console.log(tweets);
-    console.log('Success!');
+    if (!fs.existsSync(`./${getParams.screen_name}`)) {
+      fs.mkdirSync(`./${getParams.screen_name}`);
+    }
+    fs.appendFile(`./${getParams.screen_name}_${now}.json`, JSON.stringify(tweets), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   } else {
     console.log(error);
   }
